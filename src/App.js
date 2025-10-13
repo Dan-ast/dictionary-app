e0f12dec2daa591c547b2f5307cc4a7538f09a45
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2025-10-13 19:53:39 +0200

    added photos API

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Dictionary defaultKeyword="forest"/>
       </main>
       <footer className="App-footer">
-        <small>Coded by Anastasiia Dekret</small>
+        <small> This project was coded by <a href="https://github.com/Dan-ast" target="_blank">Anastasiia Dekret</a> and is open-sourced on <a href="https://github.com/Dan-ast/dictionary-app" target="_blank">GitHub</a> and hosted on <a href="https://react-dictionary-webapp.netlify.app/" target="_blank">Netlify</a>. </small>
       </footer>
     </div>
   );
